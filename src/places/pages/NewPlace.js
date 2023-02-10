0ad0abd654f1d0cf2a936604e7c609774939e20a
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-02-10 22:31:34 +0500

    validators

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { VALIDATOR_REQUIRE } from '../../shared/utils/validators';
 import Input from '../../shared/components/FormElements/Input';
 import './NewPlace.css';
 
@@ -10,7 +11,7 @@ const NewPlace = () => {
         element="input"
         type="text"
         label="Title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
       />
     </form>

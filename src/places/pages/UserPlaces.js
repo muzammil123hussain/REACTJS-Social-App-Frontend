2afb6793e5a173c0d742895701ca2b624ffc8ad7
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-03-01 18:40:12 +0500

    get places by user id

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,42 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 import PlaceList from "../components/PlaceList";
 
-
 const UserPlaces = () => {
-  const PLACES = [
-    {
-      id: "p1",
-      image:
-      "https://upload.wikimedia.org/wikipedia/commons/d/d4/View_of_Makli_by_Usman_Ghani_%28cropped%29.jpg",
-      title: "Makli Necropolis",
-      description: "Makli Necropolis is one of the largest funerary sites in the world, spread over an area of 10 kilometres near the city of Thatta, in the Pakistani province of Sindh. The site houses approximately 500,000 to 1 million tombs built over the course of a 400-year period.",
-      address: "Makli, Thatta, Sindh",
-      location: {
-        lat: 24.7518586,
-        lang: 67.8980076,
-      },
-      creator: "u1",
-    },
-    {
-      id: "p2",
-      image:
-        "https://stdc.gos.pk/media/com_solidres/assets/images/system/keenjharlake03.jpg",
-      title: "KARLI",
-      description: "Keenjhar Lake commonly called Malik Lake is located in Thatta District of Sindh the province of Pakistan. It is situated about 36 kilometres from the city of Thatta. It is the largest fresh water lake in Pakistan and an important source of drinking water for Thatta District and Karachi city.",
-      address: "Thatta, Sindh",
-      location: {
-        lat: 24.9400821,
-        lang: 68.0524276,
-      },
-      creator: "u2",
-    },
-  ];
+  const userId = useParams().userId;
+  const [loadedPlacesUser, setLoadedPlacesUser] = useState([]);
 
-  const userId =  useParams().userId;
-  const filterdPlaces = PLACES.filter(place => place.creator === userId)
-  return <PlaceList items={filterdPlaces} />;
+  const { isLoading, isError, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await sendRequest(
+          "http://localhost:5000/api/places/user/" + userId
+        );
+        console.log(response.places);
+        setLoadedPlacesUser(response.places);
+      } catch (error) {}
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
+  return (
+    <React.Fragment>
+      {isError && <ErrorModal error={isError} onClear={clearError} />}
+      {isLoading && <LoadingSpinner asOverlay />}
+      <PlaceList items={loadedPlacesUser} />
+    </React.Fragment>
+  );
 };
 
 export default UserPlaces;

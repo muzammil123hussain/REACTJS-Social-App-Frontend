7d4a18a59d999f0b19a2315ec6055cfb3bf4bbc9
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-03-10 16:33:23 +0500

    clean AP

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -50,7 +50,6 @@ const NewPlace = () => {
       formData.append("title", formState.inputs.title.value);
       formData.append("description", formState.inputs.description.value);
       formData.append("address", formState.inputs.address.value);
-      formData.append("creator", auth.userID);
       formData.append("image", formState.inputs.image.value);
 
       // eslint-disable-next-line
